6f5d2310a0b6b48c5103429a3a926a19f698dd27
frenesi-code <paulo.guitarrista@gmail.com>
2020-02-13 16:41:07 -0800

    Marco Polo exercise completed

--- js/07-marco-polo.js
+++ js/07-marco-polo.js
@@ -4,16 +4,15 @@ let multiples5 = 5;
 
 for (let i = 1; i <= iterationsNumber; i++) {
     
-    if ((i % 3 === 0) && (multiples3 += i)) {
-        console.log("Marco!      | Multiple Number Double Check => " + i);
-        
+    if ((i % 3 === 0)) {
+        console.log("Marco!      | Multiple Number Double Check => " + i);        
     }
 
-    if ((i % 5 === 0) && (multiples5 += i)) {
+    if ((i % 5 === 0)) {
         console.log("Polo!       | Multiple Number Double Check => " + i);        
     }
  
-    if (((i % 3 === 0) && (multiples3 += i)) && ((i % 5 === 0) && (multiples5 += i))) {
+    if ((i % 3 === 0) && ((i % 5 === 0))) {
         console.log("Marco Polo! | Multiple Number Double Check => " + i);                
     }
 }
