b19cf2aabd46cc5754f39cb65e04522d75a682a7
frenesi-code <paulo.guitarrista@gmail.com>
2020-02-13 18:05:52 -0800

    Validate Numeric Entry exercise completed

---
+++ js/10-validate-numeric-entry.js
@@ -0,0 +1,4 @@
+
+let investment = parseFloat(window.prompt("Enter investment amount as xxxx.xx"));
+let rate = parseFloat(window.prompt("Enter interest rate as xx.x"));
+let years = parseFloat(window.prompt("Enter number of years", 10));
